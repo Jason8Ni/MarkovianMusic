37ac6f33d0f1204cf8ebd56b96ddb6de27f4d380
jason8ni <jason8ni@hotmail.com>
2018-10-23 22:50:13 -0400

    build markov methods

--- src/markovChain.py
+++ src/markovChain.py
@@ -6,6 +6,27 @@ from collections import Counter, defaultdict, namedtuple
 
 import random
 
+import numpy as np
+
 #Define what a note is
 # consists of a note as well as the duration it is to be played
-Node = namedtuple('Node', ['note', duration])
+Note = namedtuple('Node', ['note', 'duration'])
+
+class MarkovChain: 
+
+    def __init__(self):
+        self.chain = defaultdict(Counter)
+        self.sums = defaultdict(int)
+    
+    def _serialize(self, note, duration):
+        return Note(note, duration)
+
+    def __str__(self):
+        return str(self.getChain())
+
+    def getChain(self):
+        return {k: dict(v) for k, v in self.chain.items()}
+
+    def add(self, fromNote, toNote, duration):
+        self.chain[fromNote][self._serialize(toNote, duration)] += 1
+        self.sums[fromNote] += 1
